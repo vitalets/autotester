b62903ae10f401ae2f2a8fd6fb807d152da02dcc
vitalets <vitalets@yandex-team.ru>
2016-07-31 16:26:12 +0300

    builder: copy specs-selenium

---
+++ builder/copy-specs-selenium.js
@@ -0,0 +1,24 @@
+/**
+ * Copy own selenium specs to be executed via autotester
+ */
+'use strict';
+
+const fs = require('fs-extra');
+const path = require('path');
+const paths = require('./paths');
+
+const srcDir = './node_modules/selenium-webdriver/test';
+const destDir = path.join(paths.UNPACKED, '/test/specs-selenium/');
+
+const tests = [
+  'tag_name_test.js',
+  'element_finding_test.js',
+];
+
+fs.ensureDirSync(destDir);
+tests.forEach(test => {
+  const srcFile = path.join(srcDir, test);
+  const destFile = path.join(destDir, test);
+  fs.copySync(srcFile, destFile, {clobber: true});
+  console.log(`Copied: ${srcFile} --> ${destFile}`);
+});
