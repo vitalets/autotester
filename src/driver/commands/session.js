2426d521fd257d10d2f6683ace18532a58a23f2d
vitalets <vitalets@yandex-team.ru>
2016-08-18 17:07:24 +0300

    set initial url to about:blank

--- src/driver/commands/session.js
+++ src/driver/commands/session.js
@@ -10,9 +10,12 @@ const Targets = require('../targets');
 // session id is constant as we have only one instance of chrome
 const SESSION_ID = 'autotester-session';
 
+// initial url when starting new session
+const INITIAL_URL = 'about:blank';
+
 exports.start = function () {
   Targets.reset();
-  return TabLoader.create({})
+  return TabLoader.create({url: INITIAL_URL})
     .then(tab => Targets.switchByProp('tabId', tab.id))
     .then(() => new Session(SESSION_ID, {}));
 };
