b584f52d9b3a2a6fe3ac942ea2cc65af31dffbdf
vitalets <vitalets@yandex-team.ru>
2016-08-26 13:01:06 +0300

    remove self extension urls from error stack

--- src/background/error-handler.js
+++ src/background/error-handler.js
@@ -2,8 +2,12 @@
  * Handler for uncaught errors that proxies it to ui
  */
 
+const escapeStringRegexp = require('escape-string-regexp');
+
 const TEST_PREFIX = '[TEST]: ';
 const BG_PREFIX = '[BACKGROUND]: ';
+
+let selfUrlREgexp;
 
 module.exports = function (error) {
   if (error.isMocha) {
@@ -23,6 +27,26 @@ function getViews() {
 
 function getMsg(error) {
   const prefix = error.isTestSelf ? TEST_PREFIX : BG_PREFIX;
-  const msg = error.stack || error.message || `Unknown error: ${error}`;
+  const msg = cleanStack(error.stack) || error.message || `Unknown error: ${error}`;
   return prefix + msg;
+}
+
+/**
+ * Remove 'chrome-extension://cidkhbpkgpdkadkjpkfooofilpmfneog/' part from stack as it just garbage
+ *
+ * @param {String} stack
+ */
+function cleanStack(stack) {
+  return typeof stack === 'string'
+    ? stack.replace(getSelfUrlRegexp(), '')
+    : stack;
+}
+
+function getSelfUrlRegexp() {
+  if (!selfUrlREgexp) {
+    const selfUrl = chrome.runtime.getURL('');
+    const escaped = escapeStringRegexp(selfUrl);
+    selfUrlREgexp = new RegExp(escaped, 'g');
+  }
+  return selfUrlREgexp;
 }
