59411da6bb12159fba9b27a64437005b9dedab7c
vitalets <vitalets@yandex-team.ru>
2016-09-06 21:01:36 +0300

    selftest: refactor structure

--- test/specs/index.js
+++ test/specs/index.js
@@ -15,17 +15,17 @@ module.exports = {
   tests: [
     'playground.js',
     // === specs for autotester extra features (not supported by selenium)
-    'specs-extra/newtab_switching_test.js',
-    'specs-extra/extension_switching_test.js',
-    'specs-extra/collect_network_requests_test.js',
+    'extra/newtab_switching_test.js',
+    'extra/extension_switching_test.js',
+    'extra/collect_network_requests_test.js',
     // === specs to be contributed to selenium
-    'specs-selenium-contrib/form_handling_test.js',
-    'specs-selenium-contrib/execute_async_script_test.js',
-    'specs-selenium-contrib/window_switching_test.js',
+    'selenium-contrib/form_handling_test.js',
+    'selenium-contrib/execute_async_script_test.js',
+    'selenium-contrib/window_switching_test.js',
     // === specs copied from existing selenium tests in 'node_modules/selenium-webdriver/test'
-    'specs-selenium/tag_name_test.js',
+    'selenium/tag_name_test.js',
     //'specs-selenium/actions_test.js',
-    'specs-selenium/execute_script_test.js',
+    'selenium/execute_script_test.js',
     //'specs-selenium/element_finding_test.js',
   ]
 };
