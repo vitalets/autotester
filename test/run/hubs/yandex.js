9d4f9be7223823a6d6a23532ca2a733cf0484766
vitalets <vitalets@yandex-team.ru>
2016-09-07 18:31:40 +0300

    selftest: use elements acc for yandex

--- test/run/hubs/yandex.js
+++ test/run/hubs/yandex.js
@@ -8,15 +8,13 @@ const capabilities = require('../capabilities');
 
 const targets = [
   {
-    'version' : '48.0',
+    'version' : '53.0',
+    'platform': 'WINDOWS'
   },
-  // {
-  //   'version' : '48.0',
-  // },
 ];
 
 exports.name = 'yandex';
-exports.serverUrl = `http://selenium:selenium@sg.yandex-team.ru:4444/wd/hub`;
+exports.serverUrl = `http://${process.env.YANDEX_USER}:${process.env.YANDEX_KEY}@sg.yandex-team.ru:4444/wd/hub`;
 exports.capabilities = function () {
   return capabilities.get()
     .then(caps => {
