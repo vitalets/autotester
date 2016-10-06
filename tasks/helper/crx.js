04d7eb427cbbcc5d4d90917c9b7da2d05a06fbea
vitalets <vitalets@yandex-team.ru>
2016-10-06 16:59:05 +0300

    build: fix creating crx

--- tasks/helper/crx.js
+++ tasks/helper/crx.js
@@ -6,16 +6,21 @@
 
 const Crx = require('crx');
 const fs = require('fs-extra');
+const path = require('path');
 
 exports.pack = function ({dir, key, out}) {
   const crx = new Crx({
     privateKey: fs.readFileSync(key)
   });
 
-  return crx.load(dir)
+  return crx.load(path.resolve(dir))
     .then(crx => crx.pack())
     .then(crxBuffer => {
       fs.writeFileSync(out, crxBuffer);
       console.log(`crx: packed ${dir} --> ${out}`);
-    });
+    })
+    .catch(e => {
+      console.log(e);
+      process.exit(1);
+    })
 };
