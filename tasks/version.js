891d6710022c7f8bf641f9667c0409dd31791959
vitalets <vitalets@yandex-team.ru>
2016-10-11 19:44:16 +0300

    build: version script and travis

---
+++ tasks/version.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fs = require('fs-extra');
+const {exec} = require('shelljs');
+
+const version = require('../package').version;
+const manifestPath = 'src/manifest.json';
+
+const manifest = fs.readJsonSync(manifestPath);
+manifest.version = version;
+fs.writeJsonSync(manifestPath, manifest);
+exec(`git add -A ${manifestPath}`);
+console.log(`New version bumped to ${manifestPath}: ${version}`);
