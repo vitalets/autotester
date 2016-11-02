0c7d0244403469160895c08c3550234029c54574
vitalets <vitalets@yandex-team.ru>
2016-11-02 11:52:24 +0300

    selftest: temporary increase alert delay

--- test/specs/selenium-contrib/alerts_test.js
+++ test/specs/selenium-contrib/alerts_test.js
@@ -50,7 +50,7 @@ test.suite(function(env) {
     test.it('should accept alert', function () {
       driver.findElement(By.id('alert')).click();
       // todo: use driver.wait(until.alertIsPresent()); when it will be possible
-      driver.sleep(1000);
+      driver.sleep(2000);
       driver.switchTo().alert().accept();
 
       // If we can perform any action, we're good to go
