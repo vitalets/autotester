1d4c2d78a202112eba9861bd727ee9785249f259
vitalets <vitalets@yandex-team.ru>
2016-12-10 11:59:55 +0300

    allow node 7

--- tasks/helper/utils.js
+++ tasks/helper/utils.js
@@ -9,7 +9,7 @@
  * See: http://www.marcusoft.net/2015/03/packagejson-and-engines-and-enginestrict.html
  */
 exports.ensureNode6 = function () {
-  if (process.version.indexOf('v6') !== 0) {
+  if (!process.version.match(/v6|v7/)) {
     throw new Error(`Node version >= 6 required. You have ${process.version}`);
   }
 };
